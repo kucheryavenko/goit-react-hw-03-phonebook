7f3985683109ea15243e3a00db1c4801cda30e12
Oleksandr Kucheryavenko <kucheryavenko.oleksandr@gmail.com>
2022-09-26 17:47:42 +0300

    feature/localStorage
    save and load LS

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,17 +6,29 @@ import { Filter } from 'components/Filter';
 import { Section } from 'components/Section';
 import { Title } from 'components/Title';
 import { Container } from 'components/App/App.styled';
+import storage from 'service/storage';
+
+const LS_KEY = 'contacts';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const parsedContacts = storage.load(LS_KEY);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      storage.save(LS_KEY, this.state.contacts);
+    }
+  }
 
   addContact = contact => {
     if (this.checkDublicate(contact)) {
